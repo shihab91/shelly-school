3003a3edf4cf8728af5c44b1a16268a80456fe91
unknown <shumonshihab62@gmail.com>
2021-10-12 19:19:50 +0600

    initial commit

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,29 +1,33 @@
 import React from 'react';
-import { Nav, Navbar } from 'react-bootstrap';
+import { Container, Nav, Navbar } from 'react-bootstrap';
 import { NavLink } from 'react-router-dom';
 import img from '../../images/logo.png'
 import './Header.css';
 
 const Header = () => {
     return (
-        <div className="container py-3">
-            <Navbar variant="light">
-
-                <Navbar.Brand><img className="header-logo" src={img} alt="" /></Navbar.Brand>
-                <Nav className="me-auto ms-4 navMenu">
-                    <NavLink className="mx-2  navItems" to="/home">Home</NavLink>
-                    <NavLink className="mx-2  navItems" to="/teachers">Teachers</NavLink>
-                    <NavLink className="mx-2  navItems" to="/courses">Courses</NavLink>
-                    <NavLink className="mx-2  navItems" to="/about">About us</NavLink>
-                </Nav>
-                <Nav>
-                    <NavLink to="/"><i className="fab icons fa-facebook"></i></NavLink>
-                    <NavLink to="/"><i className="fab icons fa-instagram"></i></NavLink>
-                    <NavLink to="/"><i className="fab icons fa-linkedin"></i></NavLink>
-                </Nav>
+        <>
+            <Navbar bg="light" expand="lg" className='pt-4'>
+                <Container>
+                    <Navbar.Brand href="#home"><img className="header-logo" src={img} alt="" /></Navbar.Brand>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="me-auto ms-4 navMenu">
+                            <NavLink className="mx-2  navItems" to="/home">Home</NavLink>
+                            <NavLink className="mx-2  navItems" to="/teachers">Teachers</NavLink>
+                            <NavLink className="mx-2  navItems" to="/courses">Courses</NavLink>
+                            <NavLink className="mx-2  navItems" to="/about">About us</NavLink>
+                        </Nav>
+                        <Nav className="d-flex flex-row justify-content-center">
+                            <NavLink to="/"><i className="fab icons fa-facebook"></i></NavLink>
+                            <NavLink to="/"><i className="fab icons fa-instagram"></i></NavLink>
+                            <NavLink to="/"><i className="fab icons fa-linkedin"></i></NavLink>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
             </Navbar>
             <hr />
-        </div>
+        </>
     );
 };
 
