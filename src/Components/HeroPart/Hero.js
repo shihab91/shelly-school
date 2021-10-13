945757506975ec1328ffc275db0f6dc8174c2279
unknown <shumonshihab62@gmail.com>
2021-10-13 23:38:06 +0600

    nothin

--- src/Components/HeroPart/Hero.js
+++ src/Components/HeroPart/Hero.js
@@ -5,7 +5,7 @@ const Hero = () => {
     return (
         <div className="container d-flex justify-content-center align-items-center py-5">
             <div className="hero-text  ">
-                <h1>The Smartest Way <br /> to Learn  <span>Anything</span> this is shihab</h1>
+                <h1>The Smartest Way <br /> to Learn  <span>Anything</span> this is shihab shumon</h1>
                 <small>Partly that's due to the additional context created; if you test yourself and answer incorrectly, not only are you more likely to remember the right answer after you look it up... you'll also remember that you didn't remember. (Getting something wrong is a great way to remember it the next time, especially if you tend to be hard on yourself.)
                 </small>
             </div>
